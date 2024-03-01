463f94b323fd1b4ab93d981aa1c6311d81291912
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-03-01 02:21:32 -0500

    ConfiApp login google no funciona y Interfaz de menor avanzada

--- ConfiApp/app/src/main/java/com/example/confiapp/controllers/MenorInicioActivity.kt
+++ ConfiApp/app/src/main/java/com/example/confiapp/controllers/MenorInicioActivity.kt
@@ -2,10 +2,12 @@ package com.example.confiapp.controllers
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.confiapp.R
 import com.example.confiapp.databinding.ActivityMenorInicioBinding
 import com.example.confiapp.fragments.LoginFragment
+import com.example.confiapp.fragments.MensajesMenorFragment
 import com.example.confiapp.fragments.PerfilFragment
 
 class MenorInicioActivity : AppCompatActivity() {
@@ -18,13 +20,25 @@ class MenorInicioActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        val contenedor = binding.menorFragmentContainer
+        //val contenedor = binding.menorFragmentContainer
+        val fragmentMensajes = MensajesMenorFragment()
+
         val llamarButton = binding.llamarMenorButton
         val mensajeButton = binding.mensajeMenorButton
 
-        when (llamarButton.isPressed){
-            true -> {LoginFragment()}
-            false -> {PerfilFragment()}
+        // Iniciar una transacción de fragmentos
+        val transaction = supportFragmentManager.beginTransaction()
+
+        mensajeButton.setOnClickListener {
+            transaction.replace(R.id.menorFragmentContainer, fragmentMensajes)
+            transaction.commit()
+
+            val cardView = binding.buttonsCardView
+            cardView.visibility = View.INVISIBLE
+        }
+
+        if (super.onBackPressed() ==){
+
         }
 
     }
