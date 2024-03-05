7945b5aa26b74c2cbdb4a5812daa1804d567a757
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-03-05 03:13:46 -0500

    Interfaz mejorada

--- ConfiApp/app/src/main/java/com/example/confiapp/fragments/MenorInicioFragment.kt
+++ ConfiApp/app/src/main/java/com/example/confiapp/fragments/MenorInicioFragment.kt
@@ -1,12 +1,19 @@
 package com.example.confiapp.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import com.example.confiapp.R
 import com.example.confiapp.databinding.FragmentMenorInicioBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class MenorInicioFragment : Fragment(R.layout.fragment_menor_inicio), OnBackPressedListener {
+class MenorInicioFragment : Fragment(R.layout.fragment_menor_inicio), OnBackPressedListener, OnMapReadyCallback {
     override fun onBackPressed(): Boolean {
         // Aquí, implementa lo que quieras hacer cuando se presiona atrás
         // y devuelve true si quieres que este manejo sea el final (no quiere que se llame al comportamiento por defecto del sistema)
@@ -16,10 +23,8 @@ class MenorInicioFragment : Fragment(R.layout.fragment_menor_inicio), OnBackPres
     }
 
     private lateinit var binding: FragmentMenorInicioBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var mapView: MapView
+    private var googleMap: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,12 +35,14 @@ class MenorInicioFragment : Fragment(R.layout.fragment_menor_inicio), OnBackPres
 
         val fragmentMensajes = MensajesMenorFragment()
 
-        //mostrarFragmento(fragmentMensajes)
-
         mensajeButton.setOnClickListener {
             mostrarFragmento(fragmentMensajes)
-            //cardView.visibility = View.GONE
         }
+
+        // Esto llamará a `onMapReady` cuando el mapa esté listo para usarse.
+        mapView = binding.menorMap
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
 
     }
 
@@ -44,6 +51,50 @@ class MenorInicioFragment : Fragment(R.layout.fragment_menor_inicio), OnBackPres
         transaccion?.replace(R.id.menorFragmentContainer, nuevoFragmento)
         transaccion?.addToBackStack(null)
         transaccion?.commit()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+
+        googleMap = map
+        val location = LatLng(2.4449261743007327, -76.6001259041013)
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+
+    }
+
+    // Asegúrate de sobrescribir y llamar a los métodos del ciclo de vida del MapView
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        mapView.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        mapView.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 }
